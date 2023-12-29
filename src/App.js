8cfa4af974e0c0ce74e244208ea337a540ab9477
shanistic <mus87609@gmail.com>
2023-12-29 07:14:44 +0500

    Techpulse app version 1

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import Header from './components/Header';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Services from './components/Services';
+import Footer from './components/Footer';
+
+
+
+import "./styles/App.scss"
+import "./styles/header.scss"
+import "./styles/footer.scss"
+import "./styles/home.scss"
+import "./styles/contact.scss"
+import "./styles/mediaquery.scss"
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/services' element={<Services />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+        <h1>TECHPULSE</h1>
+        <p>@all rights reserved</p>
+        </div>
+        <div>
+            <h5>Follow us</h5>
+            <div>
+                <a href="www.facebook.com/Shani" target={"blank"}>Facebook</a>
+                <a href="www.instagram.com/shanistic" target={"blank"}>Instagram</a>
+                <a href="www.github.com/shanistic" target={"blank"}>Github</a>
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
