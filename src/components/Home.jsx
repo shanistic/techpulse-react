b22ce266e79af975b9092d0674be3aa46f0ab478
shanistic <mus87609@gmail.com>
2023-12-29 13:59:54 +0500

    final

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import vg from "../assets/2.webp"
-import {AiFillGoogleCircle,AiFillAmazonCircle,AiFillYoutube,AiFillInstagram} from "react-icons/ai"
+import React from "react";
+import vg from "../assets/2.webp";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
 
-const  Home = () => {
+const Home = () => {
   return (
     <>
-    <div className='home' id='home'>
+      <div className="home" id="home">
         <main>
-            <h1>TECHPULSE</h1>
-            <p>Providing you the best services.</p>
+          <h1>TECHPULSE</h1>
+          <p>Providing you the best services.</p>
         </main>
-    </div>
-    <div className="home2">
+      </div>
+      <div className="home2">
         <img src={vg} alt="Graphics" />
         <div>
-            <p>
+          <p>
             We are your one and only solution to the tech problems you face
             every day. We are leading tech company whose aim is to increase the
             problem solving ability in children.
-            </p>
+          </p>
         </div>
+      </div>
 
-    </div>
-
-    <div className="home3" id='about'>
+      <div className="home3" id="about">
         <div>
-            <h1>Who we are?</h1>
-            <p>
+          <h1>Who we are?</h1>
+          <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
             officiis hic illo quos sit commodi, aut magnam odio eligendi? Totam
             quis officiis fugit sunt dolores nostrum tenetur iusto est odio
@@ -36,44 +40,36 @@ const  Home = () => {
             voluptas vel recusandae iste aspernatur qui quasi, commodi, atque
             ipsa dolores tempore asperiores? Dolores soluta placeat molestiae
             aperiam, ullam officia fugiat corrupti odio, suscipit ipsum cum vel
-            nemo. Ratione quo tempore fugit quaerat! Facilis beatae impedit
-            similique maxime praesentium sunt unde necessitatibus voluptates
-            ipsa, et, numquam reiciendis eveniet? Aliquid nemo nesciunt placeat,
-            qui fuga quae repellat sunt, non natus, iure ab officia rem ipsa cum
-            sed cumque corporis voluptate quibusdam nostrum quod saepe
-            </p>
+            nemo. qui fullam officia fugiat corrupti odio, suscipit ipsum cum
+            vel nemo.uga quae repellat sunt, non natus, iure ab officia rem ipsa
+            cum sed
+          </p>
         </div>
-    </div>
+      </div>
 
-    <div className="home4" id="brands">
+      <div className="home4" id="brands">
         <div>
-            <h1>Brands</h1>
-            <article>
-                <div style={{animationDelay: "0.3s"}}>
-                    <AiFillGoogleCircle />
-                    <p>Google</p>
-                    
-                </div>
-                <div style={{animationDelay: "0.5s"}}>
-                    <AiFillAmazonCircle />
-                    <p>Amazon</p>
-                    
-                </div>
-                <div style={{animationDelay: "0.7s"}}>
-                    <AiFillYoutube />
-                    <p>Youtube</p>
-                    
-                </div>
-                <div style={{animationDelay: "0.9s"}}>
-                    
-                    <AiFillInstagram />
-                    <p>Instagram</p>
-                    
-                </div>                
-            </article>
+          <h1>Brands</h1>
+          <article>
+            <div style={{ animationDelay: "0.3s" }}>
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+            <div style={{ animationDelay: "0.5s" }}>
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+            <div style={{ animationDelay: "0.7s" }}>
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+            <div style={{ animationDelay: "0.9s" }}>
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
         </div>
-    </div>
-
+      </div>
     </>
   );
 };
